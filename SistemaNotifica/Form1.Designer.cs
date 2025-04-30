84e343aabaa40557866bce3b2dc7bfd917fdcacb
weinerrogerio <weinerrogerio@gmail.com>
2025-04-30 16:24:14 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaNotifica/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace SistemaNotifica
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel1 = new Panel();
+            btnResponsiveMenu = new PictureBox();
+            nightControlBox1 = new ReaLTaiizor.Controls.NightControlBox();
+            imgLogo = new PictureBox();
+            panelResponsiveMenu = new FlowLayoutPanel();
+            panel2 = new Panel();
+            btnImportarDoc = new Button();
+            panel3 = new Panel();
+            button2 = new Button();
+            panel4 = new Panel();
+            button3 = new Button();
+            panel5 = new Panel();
+            button4 = new Button();
+            panel6 = new Panel();
+            button5 = new Button();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)btnResponsiveMenu).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)imgLogo).BeginInit();
+            panelResponsiveMenu.SuspendLayout();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            panel4.SuspendLayout();
+            panel5.SuspendLayout();
+            panel6.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(btnResponsiveMenu);
+            panel1.Controls.Add(nightControlBox1);
+            panel1.Controls.Add(imgLogo);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(927, 33);
+            panel1.TabIndex = 0;
+            // 
+            // btnResponsiveMenu
+            // 
+            btnResponsiveMenu.Image = (Image)resources.GetObject("btnResponsiveMenu.Image");
+            btnResponsiveMenu.Location = new Point(7, 0);
+            btnResponsiveMenu.Name = "btnResponsiveMenu";
+            btnResponsiveMenu.Size = new Size(38, 32);
+            btnResponsiveMenu.SizeMode = PictureBoxSizeMode.CenterImage;
+            btnResponsiveMenu.TabIndex = 3;
+            btnResponsiveMenu.TabStop = false;
+            // 
+            // nightControlBox1
+            // 
+            nightControlBox1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            nightControlBox1.BackColor = Color.Transparent;
+            nightControlBox1.CloseHoverColor = Color.FromArgb(199, 80, 80);
+            nightControlBox1.CloseHoverForeColor = Color.White;
+            nightControlBox1.DefaultLocation = true;
+            nightControlBox1.DisableMaximizeColor = Color.FromArgb(105, 105, 105);
+            nightControlBox1.DisableMinimizeColor = Color.FromArgb(105, 105, 105);
+            nightControlBox1.EnableCloseColor = Color.FromArgb(160, 160, 160);
+            nightControlBox1.EnableMaximizeButton = true;
+            nightControlBox1.EnableMaximizeColor = Color.FromArgb(160, 160, 160);
+            nightControlBox1.EnableMinimizeButton = true;
+            nightControlBox1.EnableMinimizeColor = Color.FromArgb(160, 160, 160);
+            nightControlBox1.Location = new Point(788, 0);
+            nightControlBox1.MaximizeHoverColor = Color.FromArgb(15, 255, 255, 255);
+            nightControlBox1.MaximizeHoverForeColor = Color.White;
+            nightControlBox1.MinimizeHoverColor = Color.FromArgb(15, 255, 255, 255);
+            nightControlBox1.MinimizeHoverForeColor = Color.White;
+            nightControlBox1.Name = "nightControlBox1";
+            nightControlBox1.Size = new Size(139, 31);
+            nightControlBox1.TabIndex = 2;
+            // 
+            // imgLogo
+            // 
+            imgLogo.BackgroundImageLayout = ImageLayout.None;
+            imgLogo.Image = (Image)resources.GetObject("imgLogo.Image");
+            imgLogo.Location = new Point(54, 0);
+            imgLogo.Name = "imgLogo";
+            imgLogo.Size = new Size(99, 33);
+            imgLogo.SizeMode = PictureBoxSizeMode.Zoom;
+            imgLogo.TabIndex = 2;
+            imgLogo.TabStop = false;
+            // 
+            // panelResponsiveMenu
+            // 
+            panelResponsiveMenu.BackColor = Color.Black;
+            panelResponsiveMenu.Controls.Add(panel2);
+            panelResponsiveMenu.Dock = DockStyle.Left;
+            panelResponsiveMenu.Location = new Point(0, 33);
+            panelResponsiveMenu.Name = "panelResponsiveMenu";
+            panelResponsiveMenu.Size = new Size(200, 440);
+            panelResponsiveMenu.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(btnImportarDoc);
+            panel2.Location = new Point(3, 3);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(193, 44);
+            panel2.TabIndex = 3;
+            // 
+            // btnImportarDoc
+            // 
+            btnImportarDoc.Image = (Image)resources.GetObject("btnImportarDoc.Image");
+            btnImportarDoc.ImageAlign = ContentAlignment.MiddleLeft;
+            btnImportarDoc.Location = new Point(0, 1);
+            btnImportarDoc.Name = "btnImportarDoc";
+            btnImportarDoc.Padding = new Padding(15, 0, 0, 0);
+            btnImportarDoc.Size = new Size(193, 45);
+            btnImportarDoc.TabIndex = 2;
+            btnImportarDoc.Text = "   Importar";
+            btnImportarDoc.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(button2);
+            panel3.Location = new Point(722, 81);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(193, 44);
+            panel3.TabIndex = 4;
+            // 
+            // button2
+            // 
+            button2.Image = (Image)resources.GetObject("button2.Image");
+            button2.ImageAlign = ContentAlignment.MiddleLeft;
+            button2.Location = new Point(0, 1);
+            button2.Name = "button2";
+            button2.Padding = new Padding(15, 0, 0, 0);
+            button2.Size = new Size(216, 45);
+            button2.TabIndex = 2;
+            button2.Text = "Importar";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // panel4
+            // 
+            panel4.Controls.Add(button3);
+            panel4.Location = new Point(722, 152);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(193, 44);
+            panel4.TabIndex = 5;
+            // 
+            // button3
+            // 
+            button3.Image = (Image)resources.GetObject("button3.Image");
+            button3.ImageAlign = ContentAlignment.MiddleLeft;
+            button3.Location = new Point(0, 1);
+            button3.Name = "button3";
+            button3.Padding = new Padding(15, 0, 0, 0);
+            button3.Size = new Size(216, 45);
+            button3.TabIndex = 2;
+            button3.Text = "Importar";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // panel5
+            // 
+            panel5.Controls.Add(button4);
+            panel5.Location = new Point(722, 215);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(193, 44);
+            panel5.TabIndex = 5;
+            // 
+            // button4
+            // 
+            button4.Image = (Image)resources.GetObject("button4.Image");
+            button4.ImageAlign = ContentAlignment.MiddleLeft;
+            button4.Location = new Point(0, 1);
+            button4.Name = "button4";
+            button4.Padding = new Padding(15, 0, 0, 0);
+            button4.Size = new Size(216, 45);
+            button4.TabIndex = 2;
+            button4.Text = "Importar";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // panel6
+            // 
+            panel6.Controls.Add(button5);
+            panel6.Location = new Point(722, 280);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(193, 44);
+            panel6.TabIndex = 5;
+            // 
+            // button5
+            // 
+            button5.Image = (Image)resources.GetObject("button5.Image");
+            button5.ImageAlign = ContentAlignment.MiddleLeft;
+            button5.Location = new Point(0, 1);
+            button5.Name = "button5";
+            button5.Padding = new Padding(15, 0, 0, 0);
+            button5.Size = new Size(216, 45);
+            button5.TabIndex = 2;
+            button5.Text = "Importar";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(927, 473);
+            Controls.Add(panel6);
+            Controls.Add(panel5);
+            Controls.Add(panel4);
+            Controls.Add(panel3);
+            Controls.Add(panelResponsiveMenu);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)btnResponsiveMenu).EndInit();
+            ((System.ComponentModel.ISupportInitialize)imgLogo).EndInit();
+            panelResponsiveMenu.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel3.ResumeLayout(false);
+            panel4.ResumeLayout(false);
+            panel5.ResumeLayout(false);
+            panel6.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private FlowLayoutPanel panelResponsiveMenu;
+        private PictureBox imgLogo;
+        private ReaLTaiizor.Controls.NightControlBox nightControlBox1;
+        private Button btnImportarDoc;
+        private Panel panel2;
+        private Panel panel3;
+        private Button button2;
+        private Panel panel4;
+        private Button button3;
+        private Panel panel5;
+        private Button button4;
+        private Panel panel6;
+        private Button button5;
+        private PictureBox btnResponsiveMenu;
+    }
+}
